6220220a94e412ad10da6e4a3b2bbf835d8462fc
Lizet Pena De Sola <lipenade@microsoft.com>
2018-07-17 23:51:56 -0700

    Add project files.

---
+++ Calculator.Test/CalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using MyCalculator;
+// Unit Testing FrameWork
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+
+        [TestMethod] // flags method indicates TEST
+        [TestCategory("Simple Math")] // 'Trait' UT organinzation
+        [Priority(0)] // Another 'Attribute' for catagorizing
+        public void AddSimple()
+        {
+            //ARRANGE: (set up environment to run test)
+            var calculator = new MyCalculator.Calculator();
+            //ACT: (run code we are trying to test)
+            int sum = calculator.Add(1, 2);
+            //ASSERT: (validate expected results)
+            Assert.AreEqual(3, sum);
+        }
+
+
+        [TestMethod]
+        [TestCategory("Simple Math")]
+        [Priority(0)]
+        public void SubtractSimple()
+        {
+            var calculator = new MyCalculator.Calculator();
+            int sum = calculator.Subtract(5, 15);
+            Assert.AreEqual(-10, sum);
+        }
+
+
+        [TestMethod]
+        [TestCategory("Divide")]
+        [Priority(1)]
+      //[Ignore] // Allows us to skip UT 
+        public void DivideSimple()
+        {
+            var calculator = new MyCalculator.Calculator();
+            double quotient = calculator.Divide(10, 2);
+            Assert.AreEqual(5, quotient);
+        }
+
+
+        [TestMethod]
+        [TestCategory("Divide")]
+        [ExpectedException(typeof(DivideByZeroException))]
+        [Priority(1)]
+        public void DivideByZero()
+        {
+            var calculator = new MyCalculator.Calculator();
+            calculator.Divide(10, 0);
+        }
+
+    }
+}
---
+++ MyCalculator/Calculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// This methiod takes in two integers adds them and
+        /// returns the result 
+        /// </summary>
+        /// <param name="firstValue"></param>
+        /// <param name="secondValue"></param>
+        /// <returns></returns>
+        public int Add(int firstValue, int secondValue)
+        {
+            return firstValue + secondValue;
+        }
+
+
+        /// <summary>
+        /// This method takes in to integers subtracts values and 
+        /// returns the result
+        /// </summary>
+        /// <param name="firstValue"></param>
+        /// <param name="secondValue"></param>
+        /// <returns></returns>
+        public int Subtract(int firstValue, int secondValue)
+        {
+            return firstValue - secondValue;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstValue"></param>
+        /// <param name="secondValue"></param>
+        /// <returns></returns>
+        public int Divide(int firstValue, int secondValue)
+        {
+          // change return type to int &
+          // uncomment to trigger divide by zero ex
+             return firstValue/secondValue;
+          
+          // change return type to double
+          // Dividing by zero desn't throw ex wilth code below
+          // return (double)firstValue/(double)secondValue;
+        }
+
+        // Example of Front End Code
+        public int Test(int dividend, int divisor)
+        {
+            var calculator = new Calculator();
+            int result = 0;
+            try
+            {
+                result = calculator.Divide(dividend, divisor);
+            }
+            catch (DivideByZeroException)
+            {
+                return int.MaxValue;
+            }
+            catch (Exception)
+            {
+                result = int.MinValue;
+            }
+            return result;
+        }
+    }
+}
